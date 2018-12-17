6bd06878a2e9735ddb9975d40dfee084981238db
weilu <a05111993@163.com>
2018-12-17 13:13:14 +0800

    去除allowBackup属性

--- app/src/main/java/com/weilu/pay/MainActivity.java
+++ app/src/main/java/com/weilu/pay/MainActivity.java
@@ -19,7 +19,7 @@ import io.reactivex.disposables.Disposable;
 @WXPay(BuildConfig.APPLICATION_ID)
 public class MainActivity extends AppCompatActivity {
     
-    private String sign1 ="";
+    private String sign1 ="partner=\"2088621720869834\"&seller_id=\"jingzhaozhifu3@163.com\"&out_trade_no=\"154094871710285\"&subject=\"%E7%9F%AD%E4%BF%A1%2F%E8%AF%AD%E9%9F%B3%E5%85%85%E5%80%BC%3A0.01%E5%85%83\"&body=\"%E7%9F%AD%E4%BF%A1%2F%E8%AF%AD%E9%9F%B3%E5%85%85%E5%80%BC%3A0.01%E5%85%83\"&total_fee=\"0.01\"&notify_url=\"http://ex.shoppingyizhan.com/index.php/ServicePoint/CallBack/alipay_success.html\"&service=\"mobile.securitypay.pay\"&payment_type=\"1\"&_input_charset=\"utf-8\"&it_b_pay=\"30m\"&sign=\"SoaYfQP7a3BeSsay5AtU2n2q8pW2W1ywWx6rLrEcCKN8gr4g9q0fvXPNA1Zvc73Y4jnIUKVSmzfk0aesswcxXl1pce%2BRbh8N98fuhX7nG9Oyu8f%2BD4EDvE7go1EWO7sJQmPbTB%2B6lojei5L8g2xRugu5DMWjRyLnFiq9duQd7uM%3D\"&sign_type=\"RSA\"";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
